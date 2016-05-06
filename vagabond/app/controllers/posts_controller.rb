ecb093e0aa282c4dcdd95a7dadaa10cbb0aca342
altonelli <altonelli14@gmail.com>
2016-05-05 18:23:23 -0700

    post create up

--- vagabond/app/controllers/posts_controller.rb
+++ vagabond/app/controllers/posts_controller.rb
@@ -1,2 +1,22 @@
 class PostsController < ApplicationController
+  def new
+    @post = Post.new
+  end
+  def create
+    @post = Post.new(post_params)
+    @post.save
+    if current_user.posts.push(@post)
+      flash[:notice] = "#{@post.title} created"
+      redirect_to user_post_path(current_user.first_name,@post)
+    else
+      @post.destroy
+      flash[:error] = "Something went wrong, please post again."
+      redirect_to new_post_path
+    end
+  end
+
+  private
+  def post_params
+    params.require(:post).permit(:title,:content,:photo)
+  end
 end
--- vagabond/app/models/user.rb
+++ vagabond/app/models/user.rb
@@ -1,6 +1,6 @@
 class User < ActiveRecord::Base
 
-  has_many :posts, through: :user_posts
+  has_many :posts
   has_secure_password
 
   def self.confirm(params)
