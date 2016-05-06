585898b70c52a28ea0348f33fd6692b0774ee1e5
altonelli <altonelli14@gmail.com>
2016-05-05 17:56:43 -0700

    removed user_posts table from schema

--- vagabond/db/schema.rb
+++ vagabond/db/schema.rb
@@ -11,7 +11,7 @@
 #
 # It's strongly recommended that you check this file into your version control system.
 
-ActiveRecord::Schema.define(version: 20160505222524) do
+ActiveRecord::Schema.define(version: 20160506005215) do
 
   # These are extensions that must be enabled in order to support this database
   enable_extension "plpgsql"
@@ -27,16 +27,6 @@ ActiveRecord::Schema.define(version: 20160505222524) do
 
   add_index "posts", ["user_id"], name: "index_posts_on_user_id", using: :btree
 
-  create_table "user_posts", force: :cascade do |t|
-    t.integer  "user_id"
-    t.integer  "post_id"
-    t.datetime "created_at", null: false
-    t.datetime "updated_at", null: false
-  end
-
-  add_index "user_posts", ["post_id"], name: "index_user_posts_on_post_id", using: :btree
-  add_index "user_posts", ["user_id"], name: "index_user_posts_on_user_id", using: :btree
-
   create_table "users", force: :cascade do |t|
     t.string   "email"
     t.string   "first_name"
@@ -49,6 +39,4 @@ ActiveRecord::Schema.define(version: 20160505222524) do
   end
 
   add_foreign_key "posts", "users"
-  add_foreign_key "user_posts", "posts"
-  add_foreign_key "user_posts", "users"
 end
