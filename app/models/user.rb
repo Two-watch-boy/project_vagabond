a7f8e92054421119f29f05d5af1a29fbeb0b43dd
Two-Watch-Boy <twowatchboy@gmail.com>
2016-05-06 14:24:12 -0700

    added user validations

--- app/models/user.rb
+++ app/models/user.rb
@@ -3,6 +3,13 @@ class User < ActiveRecord::Base
   has_many :posts
   has_secure_password
 
+  validates :first_name, :last_name, :email, presence: true
+  validates :first_name, length: { maximum: 255 }
+  validates :last_name, length: { maximum: 255 }
+  validates :email, length: { maximum: 255 }
+  # validates :email, uniqueness: true
+
+
   def self.confirm(params)
     @user = User.find_by({email: params[:email]})
     @user.try(:authenticate, params[:password])
