ab29148e1316b3035e735bac8ab64dee7ed9dda2
srhbnnstr <srhbnnstr@gmail.com>
2016-05-07 14:34:14 -0700

    added read more uncollapse to city posts

--- app/assets/javascripts/app.js
+++ app/assets/javascripts/app.js
@@ -11,3 +11,32 @@ $(window).bind('scroll', function () {
 
     }
 });
+
+
+jQuery(function(){
+
+    var minimized_elements = $('p.postData');
+
+    minimized_elements.each(function(){
+        var t = $(this).text();
+        if(t.length < 250) return;
+
+        $(this).html(
+            t.slice(0,250)+'<span>... </span><a href="#" class="more">Read More</a>'+
+            '<span style="display:none;">'+ t.slice(250,t.length)+' <a href="#" class="less">Less</a></span>'
+        );
+
+    });
+
+    $('a.more', minimized_elements).click(function(event){
+        event.preventDefault();
+        $(this).hide().prev().hide();
+        $(this).next().show();
+    });
+
+    $('a.less', minimized_elements).click(function(event){
+        event.preventDefault();
+        $(this).parent().hide().prev().show().prev().show();
+    });
+
+});
